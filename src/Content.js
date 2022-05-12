292c661c79c5afeab96fca92c0ed0ddd8b607004
judascrow <judascrow1@gmail.com>
2022-05-12 14:22:59 +0700

    first commit

--- src/Content.js
+++ src/Content.js
@@ -1,28 +1,31 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Dave");
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ["Bob", "Kevin", "Dave"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("You clicked it");
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
   };
 
   const handleClick2 = (name) => {
-    console.log(`${name} was clicked`);
-  };
-
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
+    console.log(count);
   };
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click it</button>
-      <button onClick={() => handleClick2("Dave")}>Click it</button>
-      <button onClick={(e) => handleClick3(e)}>Click it</button>
+      <button onClick={handleClick2}>Click it</button>
     </main>
   );
 };
