3f72443170e357c7b3b15af7803ae34cf06f8f94
Your Name <you@example.com>
2022-05-13 15:48:05 +0700

    first commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
-const Footer = () => {
-  const today = new Date();
-
-  return <footer>Copyright &copy; {today.getFullYear()}</footer>;
+const Footer = ({ length }) => {
+  return (
+    <footer>
+      <p>
+        {length} List {length === 1 ? "item" : "items"}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
